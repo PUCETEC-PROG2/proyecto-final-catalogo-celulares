b348f1e1b1f66f0a295086121f144fb23d46e8db
ferv87 <guerrero87fernando@outlook.com>
2024-08-03 18:01:41 -0500

    proyect

--- celulares/catalogo_celular/views.py
+++ celulares/catalogo_celular/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+# from pokedex.forms import PokemonFor
+# from .models import Pokemon
+# from django.shortcuts import redirect, render
+
+# #importacion de librearia de autenticacion 
+# from django.contrib.auth.views import LoginView
+# from django.contrib.auth.decorators import login_required
+
+def index(request):
+    #pokemons = Pokemon.objects.all() ## SELECT * FROM pokedex_pokemon
+   ## SELECT * FROM pokedex_pokemon ORD
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'index': index}, request))
+
+ 
+# def pokemon(request, pokemon_id):
+#     #SELECT * FROM pokedex_pokemon WHERE id='pokemon_id'
+#     pokemon = Pokemon.objects.get(id=pokemon_id)
+#     template = loader.get_template('display_pokemon.html')
+#     context = {
+#         'pokemon': pokemon
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# @login_required    
+# def add_pokemon(request):
+#     if request.method=='POST':
+#         form= PokemonFor(request.POST ,request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('pokedex:index')
+        
+#     else:
+    
+#         form = PokemonFor()
+        
+#     return render(request,"add_pokemon.html",{'form': form }) 
+
+class CustomLoginView(LoginView):
+    template_name="login.html"
+       
+
--- celulares/celulares/urls.py
+++ celulares/celulares/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for celulares project.
+URL configuration for pokedex project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static 
+from catalogo_celular import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('celulares.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
+
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
